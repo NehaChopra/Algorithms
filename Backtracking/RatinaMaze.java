fdbb91bfcb35bf79eda4eed01524345c27fd7959
neha <neha.chopra@navyuginfo.com>
2017-12-23 18:23:49 +0530

    changes

--- Backtracking/RatinaMaze.java
+++ Backtracking/RatinaMaze.java
@@ -1,4 +1,4 @@
-//http://www.geeksforgeeks.org/backttracking-set-2-rat-in-a-maze/
+//https://practice.geeksforgeeks.org/problems/rat-in-a-maze-problem/1
 package Backtracking;
 import java.util.Scanner;
 
@@ -14,9 +14,10 @@ class RatinaMaze {
 					obj.maze[index][jIndex] = input.nextInt();
 				}
 			}
-			
 			boolean visited[][] = Graph.DFSUtil(row, row, obj);
+			System.out.println();
 			printMaze(visited);
+			printMazeDirection(visited);
 		}
 	}
 	public static void printMaze(boolean visited[][]){
@@ -25,6 +26,24 @@ class RatinaMaze {
 				System.out.print(visited[index][jIndex] ? 1 + " ": 0 + " ");
 			}
 			System.out.println();
+		}
+	}
+	public static void printMazeDirection(boolean visited[][]){
+		int lastVisitedRow = 0;
+		int lastVisitedCol = 0;
+		for(int index=0; index<visited.length; index++){
+			for(int jIndex=0; jIndex<visited[index].length; jIndex++){
+				if(visited[index][jIndex] == true){
+					if(index > lastVisitedRow){
+						System.out.print("D");
+					}
+					if(jIndex > lastVisitedCol){
+						System.out.print("R");
+					}
+					lastVisitedRow = index;
+					lastVisitedCol = jIndex;
+				}
+			}
 		}
 	}
 }
@@ -37,15 +56,11 @@ class Graph{
 		this.col = col;
 		this.maze = new int[row][col];
 	}
-
 	public static boolean[][] DFSUtil(int row, int col, Graph g){
 		boolean visited[][] = new boolean[row][col];
-		
 		DFS(0, 0, g, visited);
-		
 		return visited;
 	}
-	
 	public static boolean DFS(int row, int col, Graph g, boolean visited[][]){
 		if(row == g.row -1 && col == g.col -1){
 			visited[row][col] = true;
@@ -53,24 +68,24 @@ class Graph{
 		}else{
 			if(isSafe(row, col, g)){
 				visited[row][col] = true;
-				
-				if(DFS(row + 1, col, g, visited)){// move in y direction
+				if(DFS(row + 1, col, g, visited)){// move in down direction
 					return true;
 				}
-				
-				if(DFS(row, col + 1, g, visited)){// move in x direction
+				if(DFS(row, col + 1, g, visited)){// move in right direction
 					return true;
 				}
-				
+				if(DFS(row -1, col, g, visited)){// move in left direction
+					return true;
+				}
+				if(DFS(row, col - 1, g, visited)){// move in up direction
+					return true;
+				}
 				visited[row][col] = false;
 				return false;
-				
 			}
 		}
-
 		return false;
 	}
-	
 	public static boolean isSafe(int row, int col, Graph g){
 		return ( (row >=0 && row<g.row) && (col >=0 && col<g.col) && (g.maze[row][col] == 1));
 	}
