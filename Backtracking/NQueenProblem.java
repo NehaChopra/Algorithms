87a751cfe1833b60af32ab966cc50b82b7da5602
neha <neha.chopra@navyuginfo.com>
2017-12-24 16:18:29 +0530

    backtracking

--- Backtracking/NQueenProblem.java
+++ Backtracking/NQueenProblem.java
@@ -2,54 +2,54 @@
 package Backtracking;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 class NQueenProblem {
 	public static int num;
+	public static int rowTrip = 0;
 	public static void main(String []args){
 		Scanner input = new Scanner(System.in);
 		int testCases = input.nextInt();
 		for(int index=0; index<testCases; index++){
 			num = input.nextInt();
-			System.out.println();
 			nQueenProblem();
 		}
 	}
 	public static void nQueenProblem(){
 		boolean[][] board = new boolean[num][num];
-		if(DFSUtil(0, board)){
-			printBoard(board);
-		}
-	}
-	public static void printBoard(boolean[][] board){
-		for(int index=0;index<num;index++){
-			for(int jindex=0;jindex<num;jindex++){
-				if(board[index][jindex]){
-					System.out.print("Q" + "  ");					
-				}else{
-					System.out.print("1" + "  ");	
-				}
-			}
+		Stack<Integer> stack = new Stack<Integer>();
+		if(DFSUtil(0, rowTrip, board, stack)){
+			printBoard(board,stack);
 			System.out.println();
 		}
 	}
-	public static boolean DFSUtil(int col, boolean[][] board){
+	public static void printBoard(boolean[][] board,Stack<Integer> stack){
+		System.out.print("[");
+		while(stack.size() > 0){
+			System.out.print(stack.pop() + " ");
+		}
+		System.out.print("]");
+	}
+	public static boolean DFSUtil(int col, int rowTrip, boolean[][] board, Stack<Integer> stack){
 		if(col>=num){
 			return true;
 		}
 		for(int rowIndex=0; rowIndex<num; rowIndex++){
 			if(isSafe(rowIndex, col, board)){
 				board[rowIndex][col] = true;
-				if(DFSUtil(col+1, board)){
+				stack.push(rowIndex + 1);
+				if(DFSUtil(col+1,rowTrip, board, stack)){
 					return true;
 				}
 				board[rowIndex][col] = false;
+				stack.pop();
 			}
 		}
 		return false;
 	}
 	public static boolean isSafe(int row, int col, boolean[][] board){
 		for(int colIndex=0; colIndex<col; colIndex++){
-			if(board[row][colIndex] != false){
+			if(board[row][colIndex]){
 				return false;
 			}
 		}
@@ -57,14 +57,14 @@ class NQueenProblem {
 				rowIndex>=0 && colIndex>=0; 
 				rowIndex--,colIndex--
 			){
-				if(board[rowIndex][colIndex] != false){
+				if(board[rowIndex][colIndex]){
 					return false;
 				}
 		}
 		for(int rowIndex=row+1, colIndex=col-1; 
 				rowIndex<num && colIndex>=0; 
 				rowIndex++, colIndex--){
-			if(board[rowIndex][colIndex] != false){
+			if(board[rowIndex][colIndex]){
 				return false;
 			}
 		}
---
+++ Backtracking/WordBoggle.java
@@ -0,0 +1,6 @@
+//https://practice.geeksforgeeks.org/problems/word-boggle/0
+package Backtracking;
+
+class WordBoggle {
+
+}
