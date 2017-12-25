4fe4ff94cdf56ab91764663ca6015b61dbd68aed
neha <neha.chopra@navyuginfo.com>
2017-12-25 14:19:28 +0530

    ThoughtWorks

---
+++ ThoughtWorks/GroupingCountries.java
@@ -0,0 +1,18 @@
+//https://www.geeksforgeeks.org/grouping-countries/
+package ThoughtWorks;
+
+import java.util.Scanner;
+
+class GroupingCountries {
+	public static void main(String []args){
+		Scanner input = new Scanner(System.in);
+		int testCases = input.nextInt();
+		for(int index=0; index<testCases; index++){
+			int arrayLength = input.nextInt();
+			int inputArray[] = new int[arrayLength];
+			for(int jindex=0; jindex<arrayLength; jindex++){
+				
+			}
+		}
+	}
+}
