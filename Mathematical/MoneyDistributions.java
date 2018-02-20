10bc3ed7cd0573098400cfa02493ce327ea9ed60
neha <neha.chopra@navyuginfo.com>
2018-02-20 22:07:19 +0530

    tree

---
+++ Mathematical/MoneyDistributions.java
@@ -0,0 +1,21 @@
+//https://practice.geeksforgeeks.org/problems/money-distributions/0
+package Mathematical;
+
+import java.util.Scanner;
+
+class MoneyDistributions {
+	public static void main(String []args){
+		Scanner input = new Scanner(System.in);
+		int testCases = input.nextInt();
+		for(int test = 0; test<testCases; test++){
+			int num = input.nextInt();
+			int pup = input.nextInt();
+		}
+	}
+	public static void moneyDistribution(int num, int pup){
+		int result = 0;
+		for(int index=0; index<pup; index++){
+			
+		}
+	}
+}
--- tree/PrintBinaryTreelevelsinSortedOrder.java
+++ tree/PrintBinaryTreelevelsinSortedOrder.java
@@ -1,9 +1,43 @@
 //https://practice.geeksforgeeks.org/problems/print-binary-tree-levels-in-sorted-order/0
 package tree;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 class PrintBinaryTreelevelsinSortedOrder {
+	public static void sort(int[] inputArray){
+		for(int iIndex=inputArray.length/2 - 1; iIndex>=0; iIndex--){
+			heapify(inputArray, inputArray.length, iIndex);
+		}
+		
+		for(int jIndex= inputArray.length - 1; jIndex>=0; jIndex--){
+			swap(inputArray, jIndex, 0);
+			heapify(inputArray, jIndex, 0);
+		}
+	}
+	public static void heapify(int[] inputArray, int end, int index){
+		int largest = index;
+		int left = getLeft(index);
+		int right = getRight(index);
+		if(left < end && inputArray[left] > inputArray[largest]){
+			largest = left;
+		}
+		
+		if(right < end && inputArray[right] > inputArray[largest]){
+			largest = right;
+		}
+		
+		if(largest != index){
+			swap(inputArray, index, largest);
+			heapify(inputArray, end, largest);
+		}
+	}
+	public static void swap(int[] inputArray, int index, int jIndex){
+		int temp = inputArray[index];
+		inputArray[index] = inputArray[jIndex];
+		inputArray[jIndex] = temp;
+	}
 	public static void main(String []args){
 		Scanner input = new Scanner(System.in);
 		int testCases = input.nextInt();
@@ -13,39 +47,47 @@ class PrintBinaryTreelevelsinSortedOrder {
 			for(int index=0; index<arrayLength; index++){
 				array[index] = input.nextInt();
 			}
-			sortedTreePerLevel(array, 0, 0);
+			sortedTreePerLevel(array, 0, 0, 1);
 		}
 	}
-	public static void sortedTreePerLevel(int inputArray[], int index, int lastIndex){
-		if(index==0){
-			System.out.println(inputArray[0]);
-			if(getRight(index) < inputArray.length){
-				sortedTreePerLevel(inputArray, getRight(index), index);
-			}else if(getLeft(index) < inputArray.length){
-				sortedTreePerLevel(inputArray, getLeft(index), index);
-			}else{
-				return;
-			}
-		}else{
-			if(index < inputArray.length){
-				for(int root= index; root>lastIndex; root-- ){
-					System.out.print(inputArray[root] +" ");
-				}
-				System.out.println();
-			}
-			if(getRight(index) < inputArray.length){
-				sortedTreePerLevel(inputArray, getRight(index), index);
-			}else if(getLeft(index) < inputArray.length){
-				sortedTreePerLevel(inputArray, getLeft(index), index);
-			}else{
-				return;
+	public static void sortedTreePerLevel(int inputArray[], int start, int end, int size){
+		if(start >= inputArray.length && end > inputArray.length){
+			return;
+		}
+		size = Math.min(size, inputArray.length);
+		int array[] = new int[size];
+		for(int index=0; index<array.length; index++){
+			array[index] = -1;
+		}
+		for(int index=0, jIndex=start; index<array.length && jIndex<=end; index++, jIndex++){
+			if(jIndex < inputArray.length){
+				array[index] = inputArray[jIndex]; 
 			}
 		}
+		sortAndprintArray(array);
+		sortedTreePerLevel(inputArray, getLeft(start), getRight(end), 2*size);
 	}
 	public static int getLeft(int index){
 		return (2*index + 1);
 	}
 	public static int getRight(int index){
 		return (2*index + 2);
+	}
+	public static void sortAndprintArray(int inputArray[]){
+		sort(inputArray);
+		Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+		for(int index=0; index<inputArray.length; index++){
+			if(map.get(inputArray[index]) == null){
+				map.put(inputArray[index], 1);
+			}else{
+				map.put(inputArray[index], map.get(inputArray[index]) + 1);
+			}
+			if(map.get(inputArray[index]) != null && map.get(inputArray[index]) == 1){
+				if(inputArray[index] >= 0){
+					System.out.print(inputArray[index] + " ");
+				}
+			}
+		}
+		System.out.println();
 	}
 }
