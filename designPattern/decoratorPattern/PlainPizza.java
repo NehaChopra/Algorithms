5dcb1ed659e2ff3127db2ce3468651af943feba4
neha chopra <neha.chopra@paytm.com>
2018-05-29 16:04:44 +0530

    design pattern

---
+++ designPattern/decoratorPattern/PlainPizza.java
@@ -0,0 +1,15 @@
+package designPattern.decoratorPattern;
+
+public class PlainPizza implements Pizza{
+
+	@Override
+	public String getDescription() {
+		return "Plain Base Pizza";
+	}
+
+	@Override
+	public double getCost() {
+		return 10.00;
+	}
+
+}
