657000efc967a6691563789d66147b20b6c88fb3
neha chopra <neha.chopra@paytm.com>
2018-08-22 23:03:35 +0530

    Few questions

---
+++ Hashing/Mostfrequentword.java
@@ -0,0 +1,11 @@
+//https://www.geeksforgeeks.org/frequent-word-array-strings/
+package Hashing;
+
+public class Mostfrequentword {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Hashing/winnerofanelection.java
@@ -0,0 +1,11 @@
+//https://www.geeksforgeeks.org/find-winner-election-votes-represented-candidate-names/
+package Hashing;
+
+public class winnerofanelection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
